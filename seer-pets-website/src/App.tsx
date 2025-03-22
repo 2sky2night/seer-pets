b540f5db32d0f7180ca6319bef3dc7096df3ad2e
yinming <1301680879@qq.com>
2025-03-22 16:38:15 +0800

    test: 调整数据格式

--- seer-pets-website/src/App.tsx
+++ seer-pets-website/src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import pets from './assets/pets.json';
+import petsResp from './assets/pets.json';
 
 function App() {
   const [count, setCount] = useState(0)
-  const data = pets[0];
+  const data = petsResp.pets[0];
 
   return (
     <>
