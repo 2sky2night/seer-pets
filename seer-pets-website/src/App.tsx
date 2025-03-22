f5cc3bf72d3bcd00c6cebac7ca0bc1af069bf12e
yinming <1301680879@qq.com>
2025-03-22 15:35:40 +0800

    test: 测试修改

--- seer-pets-website/src/App.tsx
+++ seer-pets-website/src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import pets from './assets/pets.json';
 
 function App() {
   const [count, setCount] = useState(0)
+  const data = pets[0];
 
   return (
     <>
       <div>
+        <div>{data.name}</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
