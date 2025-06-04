6ce8138ddd9f3d78227bca9856d43aae6ac47667
Kinght <1301680879@qq.com>
2025-06-04 23:00:25 +0800

    feat: 页头展示关于信息 && 抽离页头组件

---
+++ seer-pets-website/src/layouts/header/index.tsx
@@ -0,0 +1,52 @@
+import { useState, type FC } from "react";
+import styles from "./index.module.less";
+import { timestamp } from "../../assets/pets.json";
+import dayjs from "dayjs";
+import { Modal } from "antd";
+
+/** 上一次更新时间 */
+const updateTime = timestamp
+  ? dayjs(timestamp).format("YYYY-MM-DD HH:mm")
+  : "未知";
+
+const Header: FC = () => {
+  const [visible, setVisible] = useState(false);
+  const handleCloseModal = () => setVisible(false);
+  const handleShowModal = () => setVisible(true);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <img className={styles.logo} src="/seer-logo.jpg"></img>
+        <div className={styles.tips}>
+          <span className={styles.about} onClick={handleShowModal}>
+            关于
+          </span>
+          <div>数据更新时间：{updateTime}</div>
+        </div>
+      </header>
+      <Modal
+        title="关于"
+        open={visible}
+        footer={null}
+        width="50vh"
+        onCancel={handleCloseModal}
+      >
+        <p>一个赛尔号精灵图鉴网页，支持：</p>
+        <ul style={{ marginLeft: "20px" }}>
+          <li>精灵id排序</li>
+          <li>关键词搜索</li>
+          <li>属性筛选</li>
+        </ul>
+        <p style={{ margin: "20px 0" }}>
+          如有侵权等其他问题，请联系邮箱 1301680879@qq.com 删除😊
+        </p>
+        <a href="https://github.com/2sky2night/seer-pets" target="_blank">
+          Github上查看
+        </a>
+      </Modal>
+    </>
+  );
+};
+
+export default Header;
--- seer-pets-website/src/layouts/index.tsx
+++ seer-pets-website/src/layouts/index.tsx
@@ -1,19 +1,11 @@
 import type { FC, PropsWithChildren } from "react";
 import styles from "./index.module.less";
-import { timestamp } from "../assets/pets.json";
-import dayjs from "dayjs";
-
-const updateTime = timestamp
-  ? dayjs(timestamp).format("YYYY-MM-DD HH:mm:ss")
-  : "未知";
+import Header from "./header";
 
 const Layouts: FC<PropsWithChildren> = (props) => {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>
-        <img className={styles.logo} src="/public/seer-logo.jpg"></img>
-        <div className={styles.tips}>数据更新时间：{updateTime}</div>
-      </header>
+      <Header />
       <main className={styles.main}>{props.children}</main>
     </div>
   );
